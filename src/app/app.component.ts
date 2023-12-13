0ab0b4b6adf4b29a6ec14199a7201abe83f7d1b7
Hanna Mikulich <mikulichhanna@gmail.com>
2023-12-13 17:28:49 +0300

    add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,82 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'burgers';
+  currency = '$';
+
+  form = this.fb.group({
+    order: ["", Validators.required],
+    name: ["", Validators.required],
+    phone: ["", Validators.required],
+  });
+
+  productsData :any;
+
+  constructor(private fb: FormBuilder, private appService: AppService) {
+  }
+
+  ngOnInit(){
+   this.appService.getData().subscribe(data => this.productsData = data);
+  }
+
+  scrollTo(target: HTMLElement, burger?: any) {
+    target.scrollIntoView({behavior: "smooth"});
+    if (burger) {
+      this.form.patchValue({order: burger.title + ' (' + burger.price + ' ' + this.currency + ')'});
+    }
+  }
+
+  confirmOrder() {
+    if (this.form.valid) {
+      this.appService.sendOrder(this.form.value)
+        .subscribe(
+          {
+            next: (response: any) => {
+              alert(response.message);
+              this.form.reset();
+            },
+            error: (response) => {
+              alert(response.error.message);
+            }
+          }
+        );
+    }
+  }
+
+  changeCurrency() {
+    let newCurrency = "$";
+    let coefficient = 1;
+
+    if (this.currency === "$") {
+      newCurrency = "₽";
+      coefficient = 80;
+    } else if (this.currency === "₽") {
+      newCurrency = "BYN";
+      coefficient = 3;
+    } else if (this.currency === 'BYN') {
+      newCurrency = '€';
+      coefficient = 0.9;
+    } else if (this.currency === '€') {
+      newCurrency = '¥';
+      coefficient = 6.9;
+    }
+    this.currency = newCurrency;
+
+    this.productsData.forEach((item: any) => {
+      item.price = +(item.basePrice * coefficient).toFixed(1);
+    })
+  }
 }
+
+
